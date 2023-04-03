a40bbaa5105b34c52f397161c443fb4852a5be8a
BunSoy220 <97924546+BunSoy220@users.noreply.github.com>
2023-04-03 13:52:02 -0400

    Add files via upload

--- double_hashing.h
+++ double_hashing.h
@@ -126,7 +126,7 @@ class HashTableDouble {
   }
 
 
-  size_t SetR(size_t r){
+  void SetR(size_t r){
     r_ = r;
   }
 
@@ -156,7 +156,7 @@ class HashTableDouble {
     size_t offset = 1;
     size_t current_pos = InternalHash(x);
     size_t hash = current_pos;
-    size_t rehash = DoubleHash(hash);
+    size_t rehash = DoubleHash(x);
     ++probes_;
     while (array_[current_pos].info_ != EMPTY && array_[current_pos].element_ != x) { 
       current_pos = hash + (offset*rehash);
@@ -182,8 +182,9 @@ class HashTableDouble {
 	  Insert(std::move(entry.element_));
   }
   //second function R - (x%R)
-  size_t DoubleHash(size_t x){
-    return size_t(r_-(x%r_));
+  size_t DoubleHash(HashedObj x){
+    static std::hash<HashedObj> hf2;
+    return size_t(r_-((hf2(x) % array_.size())%r_));
   }
 
   size_t InternalHash(const HashedObj & x) const {
